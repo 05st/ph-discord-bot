e19d3058bb81e2e00e076dd7b84839d64ecd3ba0
Sami <samitimsina05@gmail.com>
2020-06-18 16:33:05 -0500

    slightly better command handling

--- src/main.rs
+++ src/main.rs
@@ -17,52 +17,48 @@ use serenity::{
 };
 use chrono::Utc;
 
+const PREFIX: &str = ">";
+
 struct Handler;
 impl EventHandler for Handler {
     fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == ">rules" {
-            let send_msg = msg.author.direct_message(&ctx, |m| {
-                m.embed(|e| {
-                    e.title("Rules");
-                    e.description("**1)** Be nice.\n**2)** Use common sense.\n**3)** If we suspect you are under 13, you will be banned.");
-                    e.footer(|f| {
-                        f.text(format!("Rules as of {}", Utc::now()))
-                    })
-                })
-            });
+        if msg.content.starts_with(PREFIX) {
+            let command: Vec<String> = String::from(&msg.content[1..]).split_ascii_whitespace().map(|s| s.to_string()).collect();
 
-            if let Err(why) = send_msg {
-                println!("Error sending message: {:?}", why);
+            match command[0].as_str() {
+                "rules" => {
+                    let message = msg.author.direct_message(&ctx, |m| {
+                        m.embed(|e| {
+                            e.title("Rules");
+                            e.description("**1)** Be nice.\n**2)** Use common sense.\n**3)** If we suspect you are under 13, you will be banned.");
+                            e.footer(|f| {
+                                f.text(format!("Rules as of {}", Utc::today()))
+                            })
+                        })
+                    });
+                    if let Err(why) = message { println!("Error sending message: {:?}", why); }
+                    if let Err(why) = msg.delete(ctx.http) { println!("Error deleting message: {:?}", why); }
+                },
+                "help" => {
+                    let message = msg.author.direct_message(&ctx, |m| {
+                        m.embed(|e| {
+                            e.title("Help");
+                            e.description("**>help** | Display this bot's commands.\n**>rules** | Display the server's rules.")
+                        })
+                    });
+                    if let Err(why) = message { println!("Error sending message: {:?}", why); }
+                    if let Err(why) = msg.delete(ctx.http) { println!("Error deleting message: {:?}", why); }
+                }
+                _ => (),
             }
-
-            msg.delete(ctx.http).unwrap();
-        } else if msg.content == ">help" {
-            let send_msg = msg.author.direct_message(&ctx, |m| {
-                m.embed(|e| {
-                    e.title("Help");
-                    e.description("**>help** | Display this bot's commands.\n**>rules** | Display the server's rules.");
-                    e
-                });
-                m
-            });
-
-            if let Err(why) = send_msg {
-                println!("Error sending message: {:?}", why);
-            }
-
-            msg.delete(ctx.http).unwrap();
         }
     }
 
     fn guild_member_addition(&self, ctx: Context, _guild_id: GuildId, member: Member) {
         let msg = member.user.read().direct_message(&ctx, |m| {
-            m.content("Welcome to the Programming Hub discord server!\nPlease make sure to read over the rules by typing **>rules**.");
-            m
+            m.content("Welcome to the Programming Hub discord server!\nPlease make sure to read over the rules by typing **>rules**.")
         });
-
-        if let Err(why) = msg {
-            println!("Error sending message: {:?}", why);
-        }
+        if let Err(why) = msg { println!("Error sending message: {:?}", why); }
     }
 
     fn ready(&self, ctx: Context, _: Ready) {
