bbbf5ffa3d70be03c6f26d1a89f2fbcf64b71c41
Sami <samitimsina05@gmail.com>
2020-06-19 13:10:03 -0500

    log moderations

--- src/commands/moderation.rs
+++ src/commands/moderation.rs
@@ -1,11 +1,12 @@
+use chrono::Utc;
 use serenity::{
     prelude::Context,
     model::{
-        guild::Member.
         channel::Message,
+        guild::Member,
+        id::ChannelId,
         Permissions,
     },
-    client::bridge::gateway::ShardId,
     framework::standard::{
         Args,
         CommandResult,
@@ -13,6 +14,11 @@ use serenity::{
     },
 };
 
+// Constants
+const LOG_CHANNEL_ID: u64 = 723287482459750501; // Where moderation logs get sent
+
+// Moderation Type Enum
+#[derive(Debug)]
 enum ModerationType {
     Kick,
     Ban,
@@ -32,8 +38,25 @@ async fn parse_member(ctx: &Context, msg: &Message, member_name: String) -> Resu
     }
 }
 
-async fn log_moderation() {
-    
+async fn log_moderation(ctx: &Context, msg: &Message, moderator_name: String, victim_name: String, moderation_type: ModerationType, reason: String) {
+    if let Some(guild_id) = msg.guild_id {
+        if let Ok(guild_channels) = guild_id.channels(&ctx).await {
+            if let Some(channel) = guild_channels.get(&ChannelId(LOG_CHANNEL_ID)) {
+                let message = channel.send_message(ctx, |m| {
+                    m.embed(|e| {
+                        e.title("Moderation");
+                        e.description(format!("**Moderator:** {}\n**Victim:** {}\n**Type:** {:?}\n**Reason:** {}", moderator_name, victim_name, moderation_type, reason));
+                        e.footer(|f| {
+                            f.text(Utc::now())
+                        })
+                    })
+                }).await;
+                if let Err(why) = message {
+                    println!("Failed to log moderation: {:?}", why);
+                }
+            }
+        }
+    }
 }
 
 #[command]
@@ -45,10 +68,12 @@ async fn kick(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
 
     match member {
         Ok(m) => {
-            m.kick_with_reason(ctx, reason);
-            log_moderation();
+            if let Err(why) = m.kick_with_reason(ctx, reason).await {
+                println!("Failed to kick member: {:?}", why);
+            }
+            log_moderation(&ctx, &msg, format!("{}#{} ({})", msg.author.name, msg.author.discriminator, msg.author.id), format!("{}#{} ({})", m.user.name, m.user.discriminator, m.user.id), ModerationType::Kick, reason.to_string()).await;
         },
-        Err(why) => println!("Failed to kick member: {:?}", why),
+        Err(why) => println!("Failed to parse member: {:?}", why),
     }
 
     Ok(())
