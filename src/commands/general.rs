ed85757bbe81558912b57f909c5377a69cc17664
Sami <samitimsina05@gmail.com>
2020-06-19 12:19:37 -0500

    rules cmd

--- src/commands/general.rs
+++ src/commands/general.rs
@@ -1,3 +1,4 @@
+use chrono::Utc;
 use serenity::{
     prelude::Context,
     model::{
@@ -18,7 +19,22 @@ async fn help(ctx: &Context, msg: &Message) -> CommandResult {
     msg.author.direct_message(ctx, |m| {
         m.embed(|e| {
             e.title("Help");
-            e.description("ok it work")
+            e.description("**>help** | Display commands.\n**>rules** | Display rules.\n\n**__Staff__**\n**>kick** | Kicks user.\n**>ban** | Bans user.")
+        })
+    }).await?;
+    msg.delete(&ctx.http).await?;
+    Ok(())
+}
+
+#[command]
+async fn rules(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.author.direct_message(ctx, |m| {
+        m.embed(|e| {
+            e.title("Rules");
+            e.description("**1)** Be nice.\n**2)** Use common sense.\n**3)** If we suspect you are under 13, you will be banned.");
+            e.footer(|f| {
+                f.text(format!("Rules as of {}", Utc::today()))
+            })
         })
     }).await?;
     msg.delete(&ctx.http).await?;
